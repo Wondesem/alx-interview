1680072877ec7b0861217059c5075a5ab9c4e8c4
Wondesem <wseminew50@gmail.com>
2023-11-28 08:08:28 -0800

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,15 +8,16 @@ Returns an empty list if n <= 0
 You can assume n will be always an integer """
 
 def pascal_triangle(n):
-    """returns empty list if n <= 0"""
-    my_list = []
-    if n <= 0:
-        return my_list
-    for i in range(n):
-        first_row = [1]
-        if my_list:
-            last_row = my_list[-1]
-            first_row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
-            first_row.append(1)
-        my_list.append(first_row)
-    return (my_list)
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
