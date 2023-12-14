e83b4f38b119888c63fabf04c107e351cb1ab460
Wondesem <wseminew50@gmail.com>
2023-12-14 18:21:05 +0300

    task 0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,16 @@ method to determine the number of occurance of a letter
 it returns an integer if n is impossible returns 0
 """
 def minOperations(n):
-    at_movement = 1
-    initial_value = 0
-    counting = 0
-    while at_movement < n:
-        if((n - at_movement) % at_movement == 0):
-            initial_value = at_movement
-            at_movement += initial_value
-            counting += 2
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
         else:
-            at_movement += initial_value
-            counting += 1
-    return counting
+            now += start
+            counter += 1
+    return counter
