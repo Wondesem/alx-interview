e6cb48933f5841a27207d702e776c4a8be4d073c
Wondesem <wseminew50@gmail.com>
2024-05-15 22:01:57 -0700

    typo error was corrected

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,57 @@
 #!/usr/bin/python3
-"""
-The app that returns all grid with water 0 and land 1
-"""
+"""Module defining isWinner function."""
 
 
-def island_perimeter(grid):
-    """
-    Finds the perimeter of island gridded
-    Args:
-        grid: 2 lists of integer with 0 (water) and 1 (land)
-    Return:
-        the perimeter of the island
-    """
+def isWinner(x, nums):
+    """Function to get who has won in prime game"""
+    mariaWinsCount = 0
+    benWinsCount = 0
 
-    per = 0
-    for i in range(len(grid)):
-        for n in range(len(grid[i])):
-            if(grid[i][n] == 1):
-                if(i <= 0 or grid[i - 1][n] == 0):
-                    per += 1
-                if(i >= len(grid) - 1 or grid[i + 1][n] == 0):
-                    per += 1
-                if(n <= 0 or grid[i][n - 1] == 0):
-                    per += 1
-                if(n >= len(grid[i]) - 1 or grid[i][n + 1] == 0):
-                    per += 1
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
 
-    return per
+        if not primesSet:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
+
+
+def is_prime(n):
+    """Returns True if n is prime, else False."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    """Returns a list of prime numbers between start and end (inclusive)."""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
