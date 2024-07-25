fe33f5f37b44eba9ed70a1851b8476ad4ac8ef82
wondesem <wseminew50@gmail.com>
2024-07-25 19:58:57 +0300

    task 0 making coin change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A program that makes coin changes
+"""
+
+def makeChange(coins, total):
+    dp = [0] + [total + 1] * total
+    for coin in coins:
+        for x in range(coin, total):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != total + 1 else -1
