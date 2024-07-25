82b7705412e726d770f4eb8eb568a09dce1b2d87
wondesem <wseminew50@gmail.com>
2024-07-25 10:13:16 -0700

    task coin change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""
-A program that makes coin changes
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
-    dp = [0] + [total + 1] * total
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for x in range(coin, total):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-
-    return dp[total] if dp[total] != total + 1 else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
