1e25d7bc031cf4b4f2b37b28c4dc6772ef12d853
wondesem <wseminew50@gmail.com>
2024-07-31 22:43:45 -0700

    white space arround operator has been corrected

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,13 +9,24 @@ def island_perimeter(grid):
     returns grid sides 0 as water and 1 as land.
     """
 
-    perim = 0
+    visited = set()
+
+    def dfs(i, j):
+        '''dfs implementation'''
+        if i >= len(grid) or j >= len(grid[0]) or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        perimeter = dfs(i, j + 1)
+        perimeter += dfs(i + 1, j)
+        perimeter += dfs(i, j - 1)
+        perimeter += dfs(i - 1, j)
+        return perimeter
+
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (i <= 0 or grid[i-1][j] == 0):
-                perim += 1
-            if(i >= len(grid) - 1 or grid[i+1][j] == 0):
-                perim += 1
-            if (j >= len(grid[i]) - 1 or grid[i][j+1] == 0):
-                perim += 1
-    return perim
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
