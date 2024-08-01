1011d0c626c8e6a6f01f6c1b38d6f800c11b9694
wondesem <wseminew50@gmail.com>
2024-07-31 22:14:54 -0700

    white space arround operator has been corrected

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,10 +12,10 @@ def island_perimeter(grid):
     perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if(i <= 0 or grid[i-1][j]==0):
+            if (i <= 0 or grid[i-1][j] == 0):
                 perim += 1
-            if(i>=len(grid) -1 or grid[i+1][j] == 0):
+            if(i >= len(grid) - 1 or grid[i+1][j] == 0):
                 perim += 1
-            if(j>=len(grid[i])-1 or grid[i][j+1]==0):
+            if (j >= len(grid[i]) - 1 or grid[i][j+1] == 0):
                 perim += 1
     return perim
