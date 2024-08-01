eeebde9f8e032f2e031525eb1190fe618634e890
wondesem <wseminew50@gmail.com>
2024-08-01 07:59:00 +0300

    initial commit of task 1

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This function calculates the perimeter of island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns grid sides 0 as water and 1 as land.
+    """
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if(i <= 0 or grid[i-1][j]==0):
+                perim += 1
+            if(i>=len(grid) -1 or grid[i+1][j] == 0):
+                perim += 1
+            if(j>=len(grid[i])-1 or grid[i][j+1]==0):
+                perim += 1
+    return perim
